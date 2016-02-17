3ba8668423960ab1a061804c43c1cbf6868356fc
zhangxx0 <377241804@qq.com>
2016-02-17 10:29:37 +0800

    多媒体部分提交

--- app/src/main/java/com/xinxin/firstcodeutil/activity/AtyChoosePic.java
+++ app/src/main/java/com/xinxin/firstcodeutil/activity/AtyChoosePic.java
@@ -1,15 +1,21 @@
 package com.xinxin.firstcodeutil.activity;
 
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.xinxin.firstcodeutil.BaseActivity;
 import com.xinxin.firstcodeutil.R;
@@ -27,6 +33,7 @@ public class AtyChoosePic extends BaseActivity {
 
     public static final int TAKE_PHOTO = 1;
     public static final int CROP_PHOTO = 2;
+    public static final int CHOOSE_PHOTO = 3;
 
     private Button takePhoto;
     private Button chooseFromAlbum;
@@ -63,26 +70,31 @@ public class AtyChoosePic extends BaseActivity {
                 startActivityForResult(intent, TAKE_PHOTO);
             }
         });
+
         chooseFromAlbum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File outputImage = new File(Environment.getExternalStorageDirectory(),
-                        "output_image.jpg");
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }
-                    outputImage.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                imageUri = Uri.fromFile(outputImage);
+//                File outputImage = new File(Environment.getExternalStorageDirectory(),
+//                        "output_image.jpg");
+//                try {
+//                    if (outputImage.exists()) {
+//                        outputImage.delete();
+//                    }
+//                    outputImage.createNewFile();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                imageUri = Uri.fromFile(outputImage);
+//                Intent intent = new Intent("android.intent.action.GET_CONTENT");
+//                intent.setType("image/*");
+//                intent.putExtra("crop", true);
+//                intent.putExtra("scale", true);
+//                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//                startActivityForResult(intent, CROP_PHOTO);
                 Intent intent = new Intent("android.intent.action.GET_CONTENT");
                 intent.setType("image/*");
-                intent.putExtra("crop", true);
-                intent.putExtra("scale", true);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, CROP_PHOTO);
+                startActivityForResult(intent,CHOOSE_PHOTO); // 打开相册
+
             }
         });
 
@@ -111,8 +123,74 @@ public class AtyChoosePic extends BaseActivity {
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    // 判断手机系统的版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        // 4.4以上
+                        handleImageOnKitKat(data);
+                    } else {
+                        // 4.4以下
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
             default:
                 break;
         }
+    }
+
+    private  void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; // 解析出数字格式的ID
+                String selection = MediaStore.Images.Media._ID+"="+id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果不是document类型的Uri，则使用普通方式处理
+            imagePath = getImagePath(uri,null);
+        }
+        // 根据图片路径显示图片
+        displayImage(imagePath);
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(this,"failed to get image",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private String getImagePath(Uri uri,String selection) {
+        String path = null;
+        // 通过Uri和selection来获取真实的图片路径；
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+
+        return path;
     }
 }
